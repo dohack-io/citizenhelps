2002573e3498b6456d86555281729496aa700eb5
pointbazaar <alex23667@gmail.com>
2019-09-28 16:22:36 +0200

    add some marker on map

--- dashboard-and-backend/citizenhelps/src/main/resources/js/myleaflet.js
+++ dashboard-and-backend/citizenhelps/src/main/resources/js/myleaflet.js
@@ -6,6 +6,13 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c']
 }).addTo( mymap );
+var myicon = L.icon({
+    iconUrl: 'https://image.flaticon.com/icons/svg/179/179386.svg',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+var marker = L.marker([51.50439, 7.52767],{icon:myicon}).addTo(mymap);
 
 var detail_description = document.getElementById("description");
 

e12d1f4efc5b103f42e59987af9f6c7341062a14
Unknown <andrey.bogomolov@tryggel.com>
2019-09-29 01:02:43 +0200

    geolocation

---
+++ client-app/client/pages/geolocation.vue
@@ -0,0 +1,42 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "geolocation",
+    data: () => ({
+      location: {}
+    }),
+    methods: {
+      showPosition: function (position) {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+
+        this.location.lat = lat;
+        this.location.lng = lng;
+        console.log("Lat-Lng:", this.location.lat, this.location.lng);
+        this.$emit('coords', this.location)
+      }
+    },
+    mounted() {
+      console.log('geolocation');
+
+      // this.running_points();
+      var lat = 0;
+      var lng = 0;
+
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+      } else {
+        alert('Browser does not support geolocation.')
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- client-app/client/pages/help.vue
+++ client-app/client/pages/help.vue
@@ -21,24 +21,33 @@
                     </v-row>
                     <v-row justify="center" align="start" style="height: 10%;" wrap>
                         <v-col cols="10">
-                            <v-btn :disabled="disabled0" @click="disabled0=true" large block>standort senden</v-btn>
+                            <v-btn :disabled="disabled0" @click="(disabled0=true,get_geolocation=true)" large block>standort
+                                senden
+                            </v-btn>
                         </v-col>
                         <v-col cols="10">
                             <v-btn :disabled="disabled1" @click="disabled1=true" large block>persönliche daten senden
                             </v-btn>
+                        </v-col>
+                        <v-col cols="10" v-if="coords">
+                            ( {{Math.round(coords.lat * 1000) /1000 }},
+                            {{Math.round(coords.lng * 1000) /1000 }})
                         </v-col>
                     </v-row>
 
 
                 </v-card>
             </v-col>
+            <Geolocation :coords="(got_coords)" v-if="get_geolocation"></Geolocation>
         </v-row>
     </v-app>
 
 </template>
 
 <script>
-  import {mdiFormatClear, mdiCallMade} from '@mdi/js'
+  import {mdiFormatClear, mdiCallMade} from '@mdi/js';
+  import Geolocation from "./geolocation.vue";
+
 
   function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s * 1000));
@@ -46,13 +55,23 @@
 
   export default {
     name: "help",
+    components: {
+      Geolocation,
+
+    },
     data: () => ({
+      get_geolocation: false,
+      coords: undefined,
       points: ".",
       calling_icon: mdiCallMade,
       disabled1: false,
       disabled0: false,
     }),
     methods: {
+      got_coords: function (val) {
+        console.log('got coords',val);
+        this.coords = val;
+      },
       close: function () {
         this.$emit('close');
       },
@@ -72,6 +91,8 @@
     },
     mounted() {
       this.running_points();
+
+
     }
   }
 </script>

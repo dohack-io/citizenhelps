0762773a852bebb4467420767b028fa51ec8a843
pointbazaar <alex23667@gmail.com>
2019-09-28 19:29:20 +0200

    fetch alerts from the api

--- dashboard-and-backend/citizenhelps/src/main/java/com/citizen/helps/controllers/api/ReportsController.java
+++ dashboard-and-backend/citizenhelps/src/main/java/com/citizen/helps/controllers/api/ReportsController.java
@@ -4,6 +4,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.http.entity.ContentType;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.vanautrui.vaquitamvc.VaquitaApp;
 import org.vanautrui.vaquitamvc.controller.VaquitaController;
 import org.vanautrui.vaquitamvc.requests.VaquitaHTTPEntityEnclosingRequest;
@@ -12,6 +18,13 @@ import org.vanautrui.vaquitamvc.responses.VaquitaHTTPResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaJSONResponse;
 import org.vanautrui.vaquitamvc.responses.VaquitaTextResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+;import static com.citizen.helps.generated.tables.Incidents.INCIDENTS;
+
 public class ReportsController extends VaquitaController {
 
     public static ObjectMapper mapper=new ObjectMapper();
@@ -19,22 +32,44 @@ public class ReportsController extends VaquitaController {
     @Override
     public VaquitaHTTPResponse handleGET(VaquitaHTTPJustRequest vaquitaHTTPJustRequest, VaquitaApp vaquitaApp) throws Exception {
 
+        ArrayNode nodes = fetchAlerts();
 
-        ArrayNode node = mapper.createArrayNode();
+        //System.out.println(nodes);
+        //System.out.println(nodes.toString().length());
 
-        node.add(makeAlert("Motorcycle Accident",5));
-        node.add(makeAlert("Train Station Vandalized",35));
-
-        return new VaquitaJSONResponse(200,node);
+        return new VaquitaJSONResponse(200,nodes);
     }
 
-    public static ObjectNode makeAlert(String title, int minutesAgo){
-        ObjectNode res = mapper.createObjectNode();
+    public static ArrayNode fetchAlerts()throws Exception{
 
-        res.put("title",title);
-        res.put("minutesago",minutesAgo);
+        ArrayNode arr = mapper.createArrayNode();
 
-        return res;
+
+        String userName = "27WJWpOHnj";
+        String password = "7E7Fllxl56";
+        String url = "jdbc:mysql://remotemysql.com:3306/27WJWpOHnj";
+
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by jOOQ, internally
+        Connection conn = DriverManager.getConnection(url, userName, password);
+            DSLContext ctx = DSL.using(conn,SQLDialect.MYSQL);
+            Result<Record> incidents = ctx.select(INCIDENTS.asterisk()).from(INCIDENTS).fetch();
+
+            for(Record r  : incidents){
+                try {
+                    ObjectNode obj = mapper.createObjectNode();
+                    obj.put("title", r.get(INCIDENTS.BESCHREIBUNG).replaceAll("ä","ae").replaceAll("ö","oe").replaceAll("ü","ue") + "");
+                    obj.put("minutesago", 5 + "");
+                    arr.add(obj);
+                    System.out.println(r.get(INCIDENTS.BESCHREIBUNG));
+                }catch (Exception e){
+                    //pass
+                }
+
+            }
+        conn.close();
+
+        return arr;
     }
 
     @Override

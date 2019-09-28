d0e6de4880530dd636a6f6099c138781d7f53d0d
Unknown <andrey.bogomolov@tryggel.com>
2019-09-28 21:25:44 +0200

    app area files

---
+++ client-app/client/pages/app/report.vue
@@ -0,0 +1,139 @@
+<template>
+    <v-app>
+        <Video style="height: 100%" @close="take_video=!take_video" v-if="take_video"></Video>
+
+        <v-row justify="space-around" align="start" v-if="!take_video">
+            <v-col cols="12">
+                <v-btn v-if="!take_video" @click="take_video=!take_video">foto/video machen</v-btn>
+            </v-col>
+            <v-col cols="12">
+                <v-switch v-model="valid" class="ma-4" label="Valid" readonly></v-switch>
+                <v-switch v-model="lazy" class="ma-4" label="Lazy"></v-switch>
+            </v-col>
+
+
+            <v-col cols="12" pa-1 ma-2>
+                <v-card pa-1>
+                    <v-form
+                            pa-1
+                            ref="form"
+                            v-model="valid"
+                            :lazy-validation="lazy"
+                    >
+                        <v-text-field
+                                v-model="name"
+                                :counter="10"
+                                :rules="nameRules"
+                                label="Name"
+                                required
+                        ></v-text-field>
+
+                        <v-text-field
+                                v-model="email"
+                                :rules="emailRules"
+                                label="E-mail"
+                                required
+                        ></v-text-field>
+
+                        <v-select
+                                v-model="select"
+                                :items="items"
+                                :rules="[v => !!v || 'Item is required']"
+                                label="Item"
+                                required
+                        ></v-select>
+
+                        <v-checkbox
+                                v-model="checkbox"
+                                :rules="[v => !!v || 'You must agree to continue!']"
+                                label="Do you agree?"
+                                required
+                        ></v-checkbox>
+
+                        <v-btn
+                                :disabled="!valid"
+                                color="success"
+                                class="mr-4"
+                                @click="validate"
+                        >
+                            Validate
+                        </v-btn>
+
+                        <v-btn
+                                color="error"
+                                class="mr-4"
+                                @click="reset"
+                        >
+                            Reset Form
+                        </v-btn>
+
+                        <v-btn
+                                color="warning"
+                                @click="resetValidation"
+                        >
+                            Reset Validation
+                        </v-btn>
+                    </v-form>
+                </v-card>
+            </v-col>
+
+
+        </v-row>
+
+    </v-app>
+
+
+</template>
+
+<script>
+  import Video from "./video.vue";
+
+  export default {
+    name: "report",
+    components: {
+      Video,
+
+    },
+    data: () => ({
+      take_video: false,
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: false,
+      lazy: false,
+    }),
+
+    methods: {
+      validate() {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true
+        }
+      },
+      reset() {
+        this.$refs.form.reset()
+      },
+      resetValidation() {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client-app/client/pages/app/video.vue
@@ -0,0 +1,257 @@
+<!--
+
+Requetes this librery:
+https://sdk.amazonaws.com/js/aws-sdk-2.16.0.min.js
+More info here:         // https://docs.aws.amazon.com/rekognition/latest/dg/image-bytes-javascript.html
+
+Designed to work with Nuxt / Vue.js / AWS Rekognition and Lambda
+
+-->
+
+
+<template>
+    <v-container>
+
+
+        <v-row column>
+            <v-row align="center" justify="center" wrap>
+                <v-col v-if="stage==='start'">
+
+                    <v-btn @click="(make_photo(),stop_video(),stage='photo_taken')">Make Photo</v-btn>
+                </v-col>
+                <v-col v-if="stage==='photo_taken'">
+                    <v-btn @click="send_picture()">Save&Send</v-btn>
+                    <v-btn @click="(stage='start',stop_video(),go())">New Image</v-btn>
+                </v-col>
+
+                <v-col cols="2" style="text-align: left">
+                    <v-btn @click="close()"> {{flipBtn?'o':'x'}}</v-btn>
+                </v-col>
+
+            </v-row>
+            <v-row justify="center" wrap>
+                <v-col pl-2 v-if="stage==='photo_taken'">
+                    <v-img contain width="100%" :height="video_height" :src="url"></v-img>
+                </v-col>
+                <v-col pl-2 v-if="stage==='start'">
+                    <video ref="video" width="100%" :height="video_height" autoplay></video>
+                </v-col>
+
+            </v-row>
+
+
+        </v-row>
+
+
+        <canvas style="display: none" ref="canvas" width="320"
+                :height="video_height"></canvas>
+    </v-container>
+
+</template>
+
+<script>
+
+  var the_stream = undefined;
+  import axios from "axios";
+
+  async function getImage({
+                            canvas,
+                            width,
+                            height,
+                            mime = 'image/jpeg',
+                            quality = 0.8,
+                          }) {
+    return new Promise(resolve => {
+      const tmpCanvas = document.createElement('canvas');
+      tmpCanvas.width = width;
+      tmpCanvas.height = height;
+
+      const ctx = tmpCanvas.getContext('2d');
+      ctx.drawImage(
+          canvas,
+          0,
+          0,
+          canvas.width,
+          canvas.height,
+          0,
+          0,
+          width,
+          height,
+      );
+
+      tmpCanvas.toBlob(resolve, mime, quality);
+    });
+  }
+
+
+  axios.defaults.headers.post = {
+    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Allow-Credentials': 'true',
+    'Content-Type': 'application/json',
+  };
+
+  export default {
+    name: "Video",
+
+    data: () => ({
+      //Stages
+      stage: 'start',
+      //Camera
+      flipBtn: false,
+      //Video
+      stream: undefined,
+      video: undefined,
+      url: undefined,
+      canvas: undefined,
+      loading: false,
+      video_width: 320,
+      video_on: false,
+      video_ratio: 320 / 240,
+
+    }),
+    methods: {
+      close: function () {
+        this.stop_video();
+        this.$emit('close');
+      },
+      //Playing Video
+
+      stop_video: function () {
+        this.video.srcObject.getVideoTracks().forEach(track => track.stop());
+        this.video_on = false;
+      },
+      go: function () {
+        if (process.client) {
+          console.log('this.$navigator.mediaDevices.getUserMedia:');
+
+          console.log('Navigator:', navigator);
+          if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            // Not adding `{ audio: true }` since we only want video now
+            navigator.mediaDevices.getUserMedia({
+                  video: true
+                })
+                .then(stream => {
+                  //FlipBtn
+                  let supports = navigator.mediaDevices.getSupportedConstraints();
+                  if (supports['facingMode'] === true) {
+                    this.flipBtn = false;
+                  } else {
+                    this.flipBtn = true;
+                  }
+                  //video.src = window.URL.createObjectURL(stream);
+                  console.log('Try to do stuff');
+                  // if (stream) {
+                  //   stream.getTracks().forEach(track => track.stop())
+                  // }
+                  the_stream = stream;
+                  let tracks = stream.getTracks();
+                  for (let i = 0; i < tracks.length; i++) {
+                    this.video_ratio = tracks[i].getSettings().aspectRatio;
+                    console.log(tracks[i].getSettings());
+                  }
+                  this.video = this.$refs.video;
+
+                  this.video.srcObject = the_stream;
+                  this.video.play();
+                  this.video_on = true;
+                  // alert(true);
+                })
+                .catch(error => {
+                  // Handle Errors here.
+                  this.loading = false;
+                  // alert(error.message);
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  console.log(errorCode, errorMessage);
+                  // ...
+                });
+            ;
+          }
+        }
+      },
+      // Sends picture to the local server
+      send_picture: function (file) {
+        let data = new FormData();
+        data.append('file', file, file.fileName);
+        axios
+            .post("http://127.0.0.1:4000/",
+                data,
+                {
+                  headers: {
+                    'accept': 'application/json',
+                    'Accept-Language': 'en-US,en;q=0.8',
+                    'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                  }
+                })
+            .then(response => {
+              console.log("Axios:", response);
+              this.loading = false;
+            })
+            .catch(error => {
+              // Handle Errors here.
+              this.loading = false;
+
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              console.log(errorCode, errorMessage);
+              // ...
+            });
+      },
+      //Making Photo
+      make_photo: async function () {
+
+        this.loading = true;
+
+        this.canvas = this.$refs.canvas;
+        this.canvas.getContext('2d').drawImage(this.video, 0, 0, this.video_width, this.video_height);
+        // Get an image dataURL from the canvas.
+        console.log('this.canvas', this.canvas);
+        let imageDataURL = this.canvas.toDataURL('image/jpg');
+        var can = this.canvas;
+        const photo = await getImage({canvas: can, width: this.video_width, height: this.video_height});
+
+        // let imageBlob = this.canvas.toBlob('image/jpg');
+        this.url = imageDataURL;
+        console.log('imageBlob', photo);
+        console.log('The image URL:', this.url);
+        this.send_picture(photo);
+        this.loading = false;
+      },
+
+    },
+    computed: {
+      device_horisontal: function () {
+        return this.$vuetify.breakpoint.width / this.$vuetify.breakpoint.height > 1
+      },
+      video_height: function () {
+        console.log(this.$vuetify.breakpoint.width, this.$vuetify.breakpoint.height);
+
+        console.log(this.video_ratio);
+        console.log(this.device_horisontal);
+        if (this.device_horisontal) {
+          return parseInt(320 / this.video_ratio)
+
+        } else {
+          return parseInt(320 * this.video_ratio)
+
+        }
+      }
+
+    },
+    created() {
+      this.go();
+      // Get access to the camera!
+
+
+      // Trigger photo take
+      // this.$refs.snap.addEventListener("click", function () {
+      //   context.drawImage(video, 0, 0, 640, 480);
+      // });
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
